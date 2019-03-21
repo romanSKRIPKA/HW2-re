85700dd0a75b2bbf8e789e041bde1c622d82e27f
crapich <crapich@gmail.com>
2019-03-21 22:44:04 +0200

    re

--- HW3/test3.js
+++ HW3/test3.js
@@ -7,30 +7,30 @@ var clocktimer;
  
 function startTime() { 
 
- let currentDate = Date.now();
- let t = currentDate - initialDate;
- pauseDate = t;
+  let currentDate = Date.now();
+  let t = currentDate - initialDate;
+  pauseDate = t;
  
- let ms = t%1000;
+  let ms = t%1000;
 
- t-=ms;
- t = Math.floor (t/1000);
- let s = t%60; 
+  t-=ms;
+  t = Math.floor (t/1000);
+  let s = t%60; 
   
- t-=s;
- t = Math.floor (t/60);
- let m = t%60; 
+  t-=s;
+  t = Math.floor (t/60);
+  let m = t%60; 
   
- t-=m;
- t = Math.floor (t/60);
- let h = t%60;
+  t-=m;
+  t = Math.floor (t/60);
+  let h = t%60;
   
- if (h<10) h='0'+h;
- if (m<10) m='0'+m;
- if (s<10) s='0'+s;
- if (ms<10) ms='0'+ms;
- clock.value = h + ':' + m + ':' + s + '.' + ms;
- clocktimer = setTimeout(startTime,1);
+  if (h<10) h='0'+h;
+  if (m<10) m='0'+m;
+  if (s<10) s='0'+s;
+  if (ms<10) ms='0'+ms;
+  clock.value = h + ':' + m + ':' + s + '.' + ms;
+  clocktimer = setTimeout(startTime,1);
  }
  
 function findTime() {
@@ -40,7 +40,9 @@ function findTime() {
     start.value = 'Pause'; 
   }
   else if (start.value === 'Pause') {
-    pauses.innerHTML = clock.value + '<br>' + pauses.innerHTML; 
+    clearTimeout(clocktimer);
+    pauses.innerHTML = clock.value + '<br>' + pauses.innerHTML;
+    start.value = 'Continue';
   }
   else if (start.value === 'Continue') {
     initialDate = Date.now() - pauseDate;
@@ -48,15 +50,11 @@ function findTime() {
     start.value = 'Pause';
   }
 }
-
- function stopTime() {
-    clearTimeout(clocktimer);
- } 
-   
- function clearField() {
-    clearTimeout(clocktimer);
-    clock.value='00:00:00.000';
-    start.value = 'Start';
-    pauses.innerHTML = '';
-  }
+    
+function clearField() {
+  clearTimeout(clocktimer);
+  clock.value='00:00:00.000';
+  start.value = 'Start';
+  pauses.innerHTML = '';
+}
 
